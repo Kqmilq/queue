a4f6803c82d8f8d39bb8538ead28e688f85e1dba
Kamil <251010@edu.p.lodz.pl>
2024-05-17 08:19:09 +0200

    a

--- queue.cpp
+++ queue.cpp
@@ -1,3 +1,50 @@
-//
+#define QUEUE_SIZE 5
+int queue_table[QUEUE_SIZE] = { 0 };
+int queue_index = 0;
+int queue_zero =0;
+
+int isQueueEmpty(void)
+{
+    return (queue_index == 0) ? 1 : 0;
+}
+
+int isQueueFull(void)
+{
+    return (queue_index == (QUEUE_SIZE)) ? 1 : 0;
+}
+
+int Pop(void)
+{
+    if (!isQueueEmpty())
+    {
+        queue_zero++;
+        queue_index--;
+        return queue_table[queue_zero - 1];
+    }
+
+    return queue_table[0];
+}
+
+int First(void)
+{
+    if (!isQueueEmpty())
+    {
+        return queue_table[queue_zero];
+    }
+
+    return queue_table[0];
+}
+
+void Push(int val)
+{
+    if (!isQueueFull())
+    {
+        queue_table[queue_index] = val;
+        queue_index++;
+        if (queue_zero != 0) {
+            queue_zero--;
+        }
+    }
+}//
 // Created by Kamil on 17.05.2024.
 //
--- queue.h
+++ queue.h
@@ -1,5 +1,9 @@
 
 #ifndef KOLEJKA_A_QUEUE_H
 #define KOLEJKA_A_QUEUE_H
-
+int isQueueEmpty(void);
+int isQueueFull(void);
+int Pop(void);
+int First(void);
+void Push(int val);
 #endif //KOLEJKA_A_QUEUE_H
